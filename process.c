24345c81111ae70f4b0d2c68565010566e46b941
Neprincessa <asya-iriska@yandex.ru>
2022-09-24 03:49:43 +0300

    modify algo

--- process.c
+++ process.c
@@ -3,15 +3,15 @@
 
 void process(int N, int M, float **vector_mn, float **matrix_nn, float **result_vectors)
 {
-    omp_set_dynamic(0);      // запретить библиотеке openmp менять число потоков во время исполнения
-    omp_set_num_threads(2);
-    
     int k = 0;
-    #pragma omp parallel for shared(vector_mn, matrix_nn) private(k)
+    int p = 0;
+    omp_set_dynamic(0);      // запретить библиотеке openmp менять число потоков во время исполнения
+    omp_set_num_threads(8);
+    #pragma omp parallel for private(vector_mn, matrix_nn, result_vectors, k, p)
     for ( k=0; k < M; k++) {
         float *vector = vector_mn[k];
         float *res_vector = result_vectors[k];
-        for (int p = 0; p < N; p++) {
+        for ( p = 0; p < N; p++) {
             float *matrix_line = matrix_nn[p];
             float res = 0;
             for (int b = 0; b < N; b++) {

67400938dbde3389b02fa4b130b680107f6172e0
Neprincessa <asya-iriska@yandex.ru>
2022-09-24 04:43:19 +0300

    add descr, experiment

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@
 ./main.bin 10 100 vector.txt matrix.txt
 ./generator.bin 10 200
 ./main.bin 10 200 vector.txt matrix.txt
-./generator.bin 10 300
-./main.bin 10 300 vector.txt matrix.txt
 ./generator.bin 10 400
 ./main.bin 10 400 vector.txt matrix.txt
-./generator.bin 10 500
-./main.bin 10 500 vector.txt matrix.txt
+./generator.bin 10 800
+./main.bin 10 800 vector.txt matrix.txt
+./generator.bin 10 1600
+./main.bin 10 1600 vector.txt matrix.txt
